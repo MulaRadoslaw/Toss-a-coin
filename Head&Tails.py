216d075c9b3f3a497af4016ab2ad1538e40e74e6
MulaRadoslaw <72382265+MulaRadoslaw@users.noreply.github.com>
2020-10-14 14:11:07 +0200

    Update Head&Tails.py
    Head&Tails 2.0
    time function
    player choice
    protection by bad input
    rolling
    round winner selection
    scoring
    game winner selection

--- Head&Tails.py
+++ Head&Tails.py
@@ -1,10 +1,16 @@
+import time
 import random
 
 def game():
 
 # greetings
     print("Welcome!")
+    time.sleep(1)
     print("Throw a coin to win a prize!")
+
+# points
+    computer_count = 0
+    user_count = 0
 
 # choice
     while True:
@@ -21,8 +27,9 @@ def game():
             continue
 
 # rolling
-        print(f'\nDrawn: {drawn}\n')
-
+        print("\nThrowing...")
+        time.sleep(1)
+        print(f'Drawn: {drawn}\n')
 
 # round winner selection
         computer_wins = 'The computer wins!'
@@ -33,8 +40,23 @@ def game():
             user_choice == 'Tails' and drawn == 'Head':
             print(computer_wins)
 
+            computer_count += 1
+
         elif user_choice == 'Head' and drawn == 'Head' or \
             user_choice == 'Tails' and drawn == 'Tails':
             print(you_win)
 
+            user_count += 1
+
+        print(f'Computer: {computer_count} - You: {user_count}')
+
+# winner selection
+        if user_count == 3 :
+            print("Player is a WINNER")
+            break
+        elif computer_count == 3:
+            print('Computer is a WINNER')
+            break
+
 game()
+input()
